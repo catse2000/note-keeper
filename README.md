055a1c6f5339b5439ab86d04068a38b69b4e844e
catse2000 <catse2000@yahoo.com>
2020-10-11 20:22:07 -0500

    add heroku

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Note Taker Starter Code
+
+## User Story
+![User Story Screenshot](./Develop/public/assets/img/user-story.png)
+
+## Acceptance Criteria
+![Acceptance Criteria Screenshot](./Develop/public/assets/img/criteria.png)
+
+## Description
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const { notes } = require('./Develop/db/db.json');
+
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if(query.text) {
+        filteredResulsts = filteredResults.filter(note => note.text === query.text);
+    }
+    return filteredResults;
+};
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
