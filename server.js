d59ad3d5f8ef0949f827aa0b65f0ffe6e48c63ff
catse2000 <catse2000@yahoo.com>
2020-10-11 21:16:42 -0500

    added functionality to create a new note

--- server.js
+++ server.js
@@ -1,12 +1,57 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 const { notes } = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if(query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if(query.text) {
+        filteredResults = filteredResults.filter(note => note.text === query.text);
+    }
+    return filteredResults;
+};
+
 function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id === id)[0];
     return result;
 };
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    return note;
+};
+
+function validateNote(note) {
+    if(!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if(!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if(req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
 
 app.get('/api/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
@@ -18,6 +63,17 @@ app.get('/api/notes/:id', (req, res) => {
     }
 });
 
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if(!validateNote(req.body)) {
+        res.status(400).send('This note is not properly formatted.');
+    }
+    else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
